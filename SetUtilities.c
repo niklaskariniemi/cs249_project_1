eb74b6c2e3a93786d6397f8385c6324f3937f13e
niklaskariniemi <niklaskariniemi@gmail.com>
2022-09-05 00:15:13 -0700

    Intialized functions

---
+++ SetUtilities.c
@@ -0,0 +1,42 @@
+#include "SetUtilities.h"
+
+void copyArray( int *dest, const int *source, int sourceSize )
+{
+
+}
+
+void displaySet( const char *setName, const int *setArr, int setArrSize )
+{
+    
+}
+
+int findIntersection( int *intersectArray, 
+                             const int *oneArray, int oneArrSize, 
+                                      const int *otherArray, int otherArrSize )
+{
+    return 0; // temp return
+}
+
+int findUnion( int *unionArray,
+                             const int *oneArray, int oneArrSize, 
+                                      const int *otherArray, int otherArrSize )
+{
+    return 0; // temp return
+}
+
+void initializeSetArray( int *intArray, 
+                     int startVal, int numItems, DataSelections dataContained )
+{
+
+}
+
+bool isInSet( const int *setArray, int size, int searchVal )
+{
+    return true; //temp return
+}
+
+bool isSubSetOf( const int *oneSet, int oneSetArrSize, 
+                          const int *psbleSubset, int psbleSubsetArrSize )
+{
+    return true; //temp return
+}

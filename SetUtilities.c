f3e7f1bece217e19010e34bf5e09233b5e8844ff
niklaskariniemi <niklaskariniemi@gmail.com>
2022-09-05 20:18:56 -0700

    Create initializeSetArray function

--- SetUtilities.c
+++ SetUtilities.c
@@ -1,4 +1,7 @@
+// header files
 #include "SetUtilities.h"
+
+// function implementations 
 
 void copyArray( int *dest, const int *source, int sourceSize )
 {
@@ -7,7 +10,7 @@ void copyArray( int *dest, const int *source, int sourceSize )
 
 void displaySet( const char *setName, const int *setArr, int setArrSize )
 {
-    
+
 }
 
 int findIntersection( int *intersectArray, 
@@ -27,7 +30,50 @@ int findUnion( int *unionArray,
 void initializeSetArray( int *intArray, 
                      int startVal, int numItems, DataSelections dataContained )
 {
+    // initialize variables
+    int index = 0, value = startVal;
 
+    // conduct processing
+
+        // check data selection flag
+        switch ( dataContained )
+        {
+        case EVEN:
+            // create array with even values
+            for ( index = 0; index <= numItems; index++)
+                {
+                    // add value to array
+                    intArray[ index ] = value;
+
+                    // create new even value
+                    value =+ 2;
+                }
+            break;
+        case ODD:
+            // create array with odd values
+            for ( index = 0; index <= numItems; index++)
+                {
+                    // add value to array
+                    intArray[ index ] = value;
+
+                    // create new odd value
+                    value =+ 2;
+                }
+            break;
+        case INCREMENTED:
+            // create array with incremented values
+            for ( index = 0; index <= numItems; index++)
+                {
+                    // add value to array
+                    intArray[ index ] = value;
+
+                    // create new incremented value
+                    value =+ 1;
+                }
+            break;
+        default:
+            break;
+        }
 }
 
 bool isInSet( const int *setArray, int size, int searchVal )
